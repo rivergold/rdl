71e8106fada1656b81be81a25f3233ddf3ad8e24
hejing <gw00260330@tech.gwm.cn>
2023-08-18 17:48:02 +0800

    Update tool

--- tools/download_huggingface_dataset.py
+++ tools/download_huggingface_dataset.py
@@ -1,5 +1,9 @@
+from pathlib import Path
 from argparse import ArgumentParser
+import shutil
 import datasets
+
+
 
 if __name__ == '__main__':
     # dataset = datasets.load_dataset(
@@ -12,12 +16,18 @@ if __name__ == '__main__':
     # cache_dir = '/home/hejing/data/opensource/huggingface/data'
     # disk_dir_path = '/home/hejing/data/opensource/huggingface/disk-data/huggan__wikiart'
 
-    dataset_name = 'Fung804/makoto-shinkai-picture'
-    cache_dir = '/home/hejing/data/opensource/huggingface/data'
-    disk_dir_path = '/home/hejing/data/opensource/huggingface/disk-data/Fung804__makoto-shinkai-picture'
+    # dataset_name = 'Fung804/makoto-shinkai-picture'
+    # cache_dir = '/home/hejing/data/opensource/huggingface/cache_data'
+    # disk_dir_path = '/home/hejing/data/opensource/huggingface/disk-data/Fung804__makoto-shinkai-picture'
+
+    dataset_name = 'ChristophSchuhmann/improved_aesthetics_6.5plus'
+    cache_dir = Path('/data/hejing/opensource/huggingface/cache_data/tmp')
+    disk_dir_path = Path('/data/hejing/aigc-data/dataset/laion/improved_aesthetics_6.5plus')
 
     dataset = datasets.load_dataset(
         dataset_name,
         cache_dir=cache_dir)
     dataset.save_to_disk(
         disk_dir_path)
+    
+    shutil.rmtree(cache_dir)
